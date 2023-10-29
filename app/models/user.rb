07ec1ade4f7987623d8e4c4e6330e119a5b0ec9d
EC2 Default User <piyopiyoblue5@gmail.com>
2023-10-29 06:53:36 +0000

    fourth commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,12 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
 
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+
+  has_many :followings, through: :relationships, source: :followed
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
 
@@ -20,4 +26,19 @@ class User < ApplicationRecord
     end
     self.profile_image.variant(resize_to_fill: [weight,height]).processed
   end
+# フォローをするときの処理
+  def follow(user_id)
+  relationships.create(followed_id: user_id)
+  end
+
+# フォローを外すときの処理
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+
+  # フォローしているか判定
+  def following?(user)
+    followings.include?(user)
+  end
+
 end
