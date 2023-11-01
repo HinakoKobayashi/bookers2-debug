b60d08466172097fe4fd87d349e8eb0a878939fc
EC2 Default User <piyopiyoblue5@gmail.com>
2023-11-01 08:10:51 +0000

    exercise6 finish

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -44,10 +44,10 @@ class UsersController < ApplicationController
     user = User.find(params[:id])
     @users = user.followings.page(params[:page]).per(3).reverse_order
   end
-    # フォロワー一覧
-    def followers
-      user = User.find(params[:id])
-      @users = user.followers.page(params[:page]).per(3).reverse_order
-    end
+  
+  def followers
+    user = User.find(params[:id])
+    @users = user.followers.page(params[:page]).per(3).reverse_order
+  end
 
 end
