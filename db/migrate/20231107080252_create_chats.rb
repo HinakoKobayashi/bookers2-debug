b9bf393bef745078fb0f73f02e0e71ec2fc212a7
EC2 Default User <piyopiyoblue5@gmail.com>
2023-11-07 08:19:14 +0000

    exrecise7b-1

--- db/migrate/20231107080252_create_chats.rb
+++ db/migrate/20231107080252_create_chats.rb
@@ -1,8 +1,9 @@
-class CreateEntries < ActiveRecord::Migration[6.1]
+class CreateChats < ActiveRecord::Migration[6.1]
   def change
-    create_table :entries do |t|
+    create_table :chats do |t|
       t.references :user, null: false, foreign_key: true
       t.references :room, null: false, foreign_key: true
+      t.text :chat
 
       t.timestamps
     end
