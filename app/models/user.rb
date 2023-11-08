5432b71631dcd359e4862438e02da7e91c3cf239
EC2 Default User <piyopiyoblue5@gmail.com>
2023-11-08 01:37:06 +0000

    exercise7c

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,6 +19,8 @@ class User < ApplicationRecord
   has_many :chats
   has_many :rooms, through: :user_rooms
 
+  has_many :group_users, dependent: :destroy
+
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
 
